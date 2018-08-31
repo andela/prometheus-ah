e94828a1c434eaf31e973aca6f83bbcb380b3771
Peter Adeoye <cwizard2011@gmail.com>
2018-08-31 18:50:51 +0100

    chore(refactor): refactor codebase to es6+
    - refactor codebase to use es6 syntax
    - create build script and other scripts in package.json
    [(Finishes) #160167671]

--- config/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  secret:
-        process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
-};
--- models/index.js
+++ models/index.js
@@ -1,28 +1,27 @@
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var config = require('../config/config.js');
-var db = {};
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import config from '../config/config';
 
+const basename = path.basename(module.filename);
+const db = {};
 
+let sequelize;
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
