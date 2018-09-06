c6c77d0e6f54d04a7cdc05f0a6bbc98777e6ca9b
Peter Adeoye <cwizard2011@gmail.com>
2018-09-06 19:41:19 +0100

    feat(userController): implement get all user profile
    - write tests to confirm the get all user profile endpoint is functional server/test/profile.spec.js
    - write code to implement the functionality of the endpoint in server/controllers/UserController.js
    - implement functionality to find all users in a database for a logged in user
    - add validation method in server/middleware/AuthValidation.js
    - add documentation in server/docs/swagger/profile.yaml
    
    [Finishes #159987746]

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -27,27 +27,28 @@ class UserController {
     const password = bcrypt.hashSync(req.body.user.password, 10);
     User.find({
       where: {
-        [Op.or]: [{ username }, { email }]
-      }
+        [Op.or]: [{ username }, { email }],
+      },
     }).then((existingUser) => {
       if (existingUser) {
         return res.status(409).json({
-          message: 'Email or Username is already in use by another User.'
+          message: 'Email or Username is already in use by another User.',
         });
       }
-      User
-        .create({
-          firstname,
-          lastname,
-          username,
-          email,
-          password,
-          bio
-        })
+      User.create({
+        firstname,
+        lastname,
+        username,
+        email,
+        password,
+        bio,
+      })
         .then((user) => {
-          const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '24h' });
+          const token = jwt.sign({ userId: user.id }, secret, {
+            expiresIn: '24h',
+          });
           res.status(201).json({
-            user: { ...user.toAuthJSON(), token }
+            user: { ...user.toAuthJSON(), token },
           });
         })
         .catch(next);
@@ -65,31 +66,89 @@ class UserController {
     const { username, password } = req.body.user;
     User.findOne({
       where: {
-        username
+        username,
       },
-    }).then((user) => {
-      if (user) {
-        if (bcrypt.compareSync(password, user.password)) {
-          const token = jwt.sign({
-            userId: user.id,
-            username: user.username,
-          }, secret, { expiresIn: '24h' });
-          return res.status(200).json({
-            message: 'Welcome User you are now logged in.',
-            user: {
-              email: user.email,
-              token
-            }
+    })
+      .then((user) => {
+        if (user) {
+          if (bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(
+              {
+                userId: user.id,
+                username: user.username,
+              },
+              secret,
+              { expiresIn: '24h' }
+            );
+            return res.status(200).json({
+              message: 'Welcome User you are now logged in.',
+              user: {
+                email: user.email,
+                token,
+              },
+            });
+          }
+          return res.status(401).json({
+            message: 'Username or password does not match.',
           });
         }
-        return res.status(401).json({
-          message: 'Username or password does not match.'
+        return res.status(404).json({
+          message: 'Username or password does not match.',
         });
-      }
-      return res.status(404).json({
-        message: 'Username or password does not match.'
+      })
+      .catch(next);
+  }
+
+  /**
+   * Get all users profile
+   * @param {*} req - query parameters
+   * @param {*} res - Response object
+   * @param {*} next - Next function
+   * @returns {object} user - User object
+   */
+  static getAllUserProfile(req, res, next) {
+    const page = parseInt((req.query.page || 1), 10);
+    const limit = parseInt((req.query.limit || 10), 10);
+    const offset = parseInt((page - 1), 10) * limit;
+    let order = req.query.order || 'ASC';
+    order = order.toUpperCase();
+
+    if (order !== 'ASC' && order !== 'DESC') {
+      return res.status(400).json({
+        message: 'order can only be ASC or DESC'
       });
-    }).catch(next);
+    }
+
+    User.findAndCountAll({
+      order: [
+        ['createdAt', order]
+      ],
+      attributes: ['username', 'email', 'firstname', 'lastname', 'bio'],
+      offset,
+      limit,
+    })
+      .then((users) => {
+        const { count } = users;
+        const pageCount = Math.ceil(count / limit);
+        if (users.rows.length === 0) {
+          return res.status(200).json({
+            message: 'No user on this page',
+            users: users.rows,
+          });
+        }
+        return res.status(200).json({
+          message: 'Users profile successfully retrieved',
+          paginationMeta: {
+            pageCount,
+            totalCount: count,
+            outputCount: users.rows.length,
+            pageSize: limit,
+            currentPage: page,
+          },
+          users: users.rows,
+        });
+      })
+      .catch(next);
   }
 }
 
--- server/test/index.spec.js
+++ server/test/index.spec.js
@@ -1,1 +1,2 @@
 import './auth.spec';
+import './profile.spec';
