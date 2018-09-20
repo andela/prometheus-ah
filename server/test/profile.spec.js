7a619c68fb9274df5edd168f17e8d93cd33d7c9f
Peter Adeoye <cwizard2011@gmail.com>
2018-09-20 21:35:21 +0100

    feat(user password update): Implement method for password update
    - Create method for updating password
    - Write test for endpoint
    - Update documentation
    
    [Finishes #160642004]

--- server/test/profile.spec.js
+++ server/test/profile.spec.js
@@ -6,7 +6,8 @@ import users from '../database/seed-data/users';
 chai.use(chaiHttp);
 
 let userToken;
-describe('Get All User Profile', () => {
+let userToken2;
+describe('Users profile and password update', () => {
   before((done) => {
     chai.request(app)
       .post('/api/users/login')
@@ -20,6 +21,25 @@ describe('Get All User Profile', () => {
       })
       .end((err, res) => {
         userToken = res.body.user.token;
+        if (err) return done(err);
+        done();
+      });
+  });
+  before((done) => {
+    chai.request(app)
+      .post('/api/users')
+      .set('Accept', 'application/json')
+      .send({
+        user: {
+          username: 'changepass',
+          password: 'newpassword',
+          password_confirmation: 'newpassword',
+          email: 'password@password.com'
+        }
+
+      })
+      .end((err, res) => {
+        userToken2 = res.body.user.token;
         if (err) return done(err);
         done();
       });
@@ -80,6 +100,152 @@ describe('Get All User Profile', () => {
           expect(res.body).to.have.property('paginationMeta');
           expect(res.body.paginationMeta.currentPage).to.equal(1);
           expect(res.body.paginationMeta.pageSize).to.equal(1);
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+  describe('When User is not authenticated', () => {
+    it('should return authentication error', (done) => {
+      const oldPassword = 'newpassword';
+      const password = 'password';
+      const password_confirmation = 'password'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .send({
+          oldPassword,
+          password,
+          password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('You need to signup or login to perform this action');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should throw an error if password and confirm password does not match.', (done) => {
+      const oldPassword = 'newpassword';
+      const password = 'password';
+      const password_confirmation = '1234578'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.errors.password[0]).to.equal('Password Mismatch.');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should throw an error if password is not up to 8 character', (done) => {
+      const oldPassword = 'newpassword';
+      const password = 'pass';
+      const password_confirmation = 'pass'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.errors.password[0])
+            .to.equal('The password is too short. Min length is 8 characters.');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should throw an error if password fields are empty', (done) => {
+      const oldPassword = '   ';
+      const password = '   ';
+      const password_confirmation = '  '; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.errors.password[0])
+            .to.equal('password field is required.');
+          expect(res.body.errors.oldPassword[0])
+            .to.equal('The oldPassword field is required.');
+          expect(res.body.errors.password_confirmation[0])
+            .to.equal('The password confirmation field is required.');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should throw an error if the oldPassword is wrong', (done) => {
+      const oldPassword = 'password';
+      const password = 'newpassword';
+      const password_confirmation = 'newpassword'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body.message)
+            .to.equal('password incorrect, try again');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should throw an error if the oldPassword and new password are the same', (done) => {
+      const oldPassword = 'newpassword';
+      const password = 'newpassword';
+      const password_confirmation = 'newpassword'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(409);
+          expect(res.body.message)
+            .to.equal('New password is the same as previous password');
+          expect(res.body).not.to.have.property('user');
+          if (err) return done(err);
+          done();
+        });
+    });
+    it('It should change users password when all fields are supplied correctly', (done) => {
+      const oldPassword = 'newpassword';
+      const password = 'password1234';
+      const password_confirmation = 'password1234'; // eslint-disable-line
+      chai.request(app)
+        .put('/api/users/password')
+        .set('Content-Type', 'application/json')
+        .set('authorization', userToken2)
+        .send({
+          oldPassword, password, password_confirmation
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.message)
+            .to.equal('Password successfully updated, you can now login with your new password');
+          expect(res.body).to.have.property('user');
+          expect(res.body.user).to.be.an('object');
           if (err) return done(err);
           done();
         });
